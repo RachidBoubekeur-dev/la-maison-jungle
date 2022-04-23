7cb6959b6dcfdd3f452fe7e35a9773dc52759205
Rachid Boubekeur <rachidboubekeur@free.fr>
2022-04-23 16:06:08 +0200

    add component ShoppingList

---
+++ src/components/ShoppingList/index.jsx
@@ -0,0 +1,31 @@
+import { plantList } from '../../datas/plantList';
+import './index.css';
+
+const ShoppingList = () => {
+
+    const categories = plantList.reduce(
+        (acc, plant) => acc.includes(plant.category) ? acc : acc.concat(plant.category), []
+    );
+
+    return (
+        <div>
+            <ul>
+                {categories.map((cat) => (
+                    <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+            <ul className='lmj-plant-list'>
+                {plantList.map((plant) => (
+                    <li key={plant.id} className='lmj-plant-item'>
+                        {plant.name}
+                        {plant.isBestSale && <span>🔥</span>}
+                        {plant.isSpecialOffer && <span className='lmj-sales'>Soldes</span>}
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+    );
+};
+
+export default ShoppingList;;
